741512b4119eed91be4bd1f4bfcd843dc2d533ec
jlbuild <juliabuildbot@gmail.com>
2021-12-18 18:05:55 +0000

    gdk_pixbuf_jll build 2.42.6+1

--- src/wrappers/i686-linux-musl.jl
+++ src/wrappers/i686-linux-musl.jl
@@ -8,8 +8,8 @@ using Libtiff_jll
 using Xorg_libX11_jll
 JLLWrappers.@generate_wrapper_header("gdk_pixbuf")
 JLLWrappers.@declare_file_product(gdk_pixbuf_loaders_dir)
-JLLWrappers.@declare_executable_product(gdk_pixbuf_query_loaders)
 JLLWrappers.@declare_library_product(libgdkpixbuf, "libgdk_pixbuf-2.0.so.0")
+JLLWrappers.@declare_executable_product(gdk_pixbuf_query_loaders)
 function __init__()
     JLLWrappers.@generate_init_header(Glib_jll, JpegTurbo_jll, libpng_jll, Libtiff_jll, Xorg_libX11_jll)
     JLLWrappers.@init_file_product(
@@ -17,15 +17,15 @@ function __init__()
         "lib/gdk-pixbuf-2.0/2.10.0/loaders",
     )
 
-    JLLWrappers.@init_executable_product(
-        gdk_pixbuf_query_loaders,
-        "bin/gdk-pixbuf-query-loaders",
-    )
-
     JLLWrappers.@init_library_product(
         libgdkpixbuf,
         "lib/libgdk_pixbuf-2.0.so",
         RTLD_LAZY | RTLD_DEEPBIND,
+    )
+
+    JLLWrappers.@init_executable_product(
+        gdk_pixbuf_query_loaders,
+        "bin/gdk-pixbuf-query-loaders",
     )
 
     JLLWrappers.@generate_init_footer()
